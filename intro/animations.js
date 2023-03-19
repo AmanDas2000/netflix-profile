a95182581b539fac6219a714a2759b68845cb1b0
Aman Das <das.aman45@gmail.com>
2023-03-19 15:36:45 +0530

    testing animation on deploy-3

--- intro/animations.js
+++ intro/animations.js
@@ -1,22 +1,7 @@
 const animate=()=>{
-
-    
-    const stick1 = document.getElementsByClassName('color')[0]
-    const stick2 = document.getElementsByClassName('color2')[0]
-    const stick3 = document.getElementsByClassName('brush3')[0]
     const lines = document.getElementsByClassName('lines')[0]
     
-    // make reverse animations trigger
     setTimeout(() => {
-        stick2.style.animation = 'movedown 0.5s ease forwards'
-    }, 3000)
-    
-    setTimeout(() => {
-        stick3.style.animation = 'movedown 0.5s ease forwards'
-    }, 3500)
-    
-    setTimeout(() => {
-        stick1.style.animation = 'opacity 0.5s ease forwards'
         lines.style.display = 'flex'
     }, 4000)
     
