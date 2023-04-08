bccb204c7f18da2ef3611e5244a67071d77356a2
Aman Das <das.aman45@gmail.com>
2023-04-08 17:53:09 +0530

    intro shift to component-1

--- intro/Intro.tsx
+++ intro/Intro.tsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Intro = () => {
+  const [index, setIndex] = useState(false)
+  useEffect(() => {
+    const intervalDelayMilliseconds = 4000
+    const interval = setInterval(() => {
+      setIndex(true)
+    }, intervalDelayMilliseconds)
+
+    return () => clearInterval(interval)
+  })
+
+  // lets create some random lines width random margins or gaps
+  const randomHexColorCode = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16)
+    return '#' + n.slice(0, 6)
+  }
+
+  const lineElements: JSX.Element[] = []
+
+  const randomColors: string[] = []
+
+  for (let i = 0; i < 50; i++) {
+    randomColors.push(randomHexColorCode())
+  }
+  randomColors.map((color) => {
+    const randomMargin = Math.floor(Math.random() * 1000)
+
+    lineElements.push(
+      <div className='line' style={{'--m':`${randomMargin}`,'--c':`${color}`}}>hsadsasd</div>
+    )
+  })
+
   return (
     <div>
       <div className="background">
         <div className="logo">
           <div className="brush1">
             <div className="color"></div>
-            <div className="lines"></div>
+            <div className="lines flex">
+              {index ? lineElements.map((e) => e) : null}
+            </div>
           </div>
           <div className="brush2">
             <div className="color2"></div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -183,7 +183,7 @@ const Home: NextPage = () => {
 
 
   useEffect(() => {
-    animate();
+    // animate();
   }, [])
   
   
