e730dbb79fb0dc17e5549d90f8807ab581eb5fd8
Aman Das <das.aman45@gmail.com>
2023-04-12 16:46:35 +0530

    sample data

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,28 +10,26 @@ import useWindowDimensions from '../hooks/useWindowDimension'
 import parse from 'html-react-parser';
 import Intro from '../intro/Intro'
 import Footer from '../components/Footer'
+// import {arr} from '../personal-info/info'
+
 
 const arr = [
   {
     name: 'Oracle',
-    designation: 'Software Engineer 1',
-    detail: `Front-end<br/>
-      • Developed complete gold purchase website from scratch. <a className='membershipLink' href='https://gold.spennyapp.com'>link</a><br/>
-      • Handled authentication, gold purchase APIs, payments and edge cases using Nextjs, React and Redux<br/>
-      • Built completely responsive UI/UX using CSS and theme-ui<br/>
-      Back-end<br/>
-      • Designed micro-service architecture for different modules<br/>
-      • Asset baskets include queue architecture (rabbit-mq) for efficient order placement<br/>
-      • Implemented production level purchase and sell rest APIs with node.js and express<br/>
-      • Worked on baskets and SIP module<br/>`,
+    designation: 'Software Engineering Intern',
+    detail: `• Build the "Robot-server" which provides a all-in-one test running platform.<br/>
+    • Created the UI which facilitates to check multiple microservices.<br/>
+    • Worked on the Vendor-side simulation tool which improves delivery times by 1 to 2 months.<br/>
+    • Deployed the entire simulation tool and the robot-server as a microservice using Docker and Kubernetes.<br/>
+    • The languages used in the above projects include Python, Java and robot-framework<br/>`,
     imageUrl:
       'https://imageio.forbes.com/specials-images/imageserve/5ff3492e056b3292f9c39749/0x0.jpg?format=jpg&width=1200',
-    startDate:'Mar 21',
-  endDate:'Arp 22'
+    startDate:'Jan 23',
+  endDate:'Present'
   },
   {
-    name: 'Microsoft',
-    designation: 'Software Engineer 1',
+    name: 'Spenny (YC 2020)',
+    designation: 'Full stack Intern',
     detail: `Front-end<br/>
     • Developed complete gold purchase website from scratch. <a className='membershipLink' href='https://gold.spennyapp.com'>link</a><br/>
     • Handled authentication, gold purchase APIs, payments and edge cases using Nextjs, React and Redux<br/>
@@ -42,58 +40,43 @@ const arr = [
     • Implemented production level purchase and sell rest APIs with node.js and express<br/>
     • Worked on baskets and SIP module<br/>`,
     imageUrl:
-      'https://www.cnet.com/a/img/resize/f92ae43457ac52e0b761737181264a82aa0765bb/hub/2019/02/04/8311b046-6f2b-4b98-8036-e765f572efad/msft-microsoft-logo-2-3.jpg?auto=webp&fit=crop&height=675&width=1200',
+      'https://i.ytimg.com/vi/h8d70SFOqZ4/maxresdefault.jpg',
       startDate:'Mar 21',
       endDate:'Arp 22'
     },
   {
-    name: 'Google',
-    designation: 'Software Engineer 1',
-    detail: `Front-end<br/>
-    • Developed complete gold purchase website from scratch. <a className='membershipLink' href='https://gold.spennyapp.com'>link</a><br/>
-    • Handled authentication, gold purchase APIs, payments and edge cases using Nextjs, React and Redux<br/>
-    • Built completely responsive UI/UX using CSS and theme-ui<br/>
-    Back-end<br/>
-    • Designed micro-service architecture for different modules<br/>
-    • Asset baskets include queue architecture (rabbit-mq) for efficient order placement<br/>
-    • Implemented production level purchase and sell rest APIs with node.js and express<br/>
-    • Worked on baskets and SIP module<br/>`,
+    name: 'Flodata Analytics',
+    designation: 'Frontend Intern',
+    detail: `• Worked on Delhi LDRA map dashboard.<br/>
+    • Rendered polygons, tool tips and colours according to parameters using leafletjs, React-leaflet, React and css<br/>
+    • Developed US ancestry map dashboard using AnyCharts and vanilla JavaSfcript<br/>`,
     imageUrl:
-      'https://dam.nmhmedia.sk/image/cd21770f-6da6-45d1-957a-7c183cb556f6_phpbbgdhj.jpg/960/540',
+      'https://media.licdn.com/dms/image/C4D16AQH3A6VmJZoutQ/profile-displaybackgroundimage-shrink_200_800/0/1654535134753?e=2147483647&v=beta&t=oFfzbVd319269JS3IDNbstPjMpuwST0zlgieIIabnzY',
       startDate:'Mar 21',
   endDate:'Arp 22'
   },
   {
-    name: 'Apple',
-    designation: 'Software Engineer 1',
-    detail: `Front-end<br/>
-    • Developed complete gold purchase website from scratch. <a className='membershipLink' href='https://gold.spennyapp.com'>link</a><br/>
-    • Handled authentication, gold purchase APIs, payments and edge cases using Nextjs, React and Redux<br/>
-    • Built completely responsive UI/UX using CSS and theme-ui<br/>
-    Back-end<br/>
-    • Designed micro-service architecture for different modules<br/>
-    • Asset baskets include queue architecture (rabbit-mq) for efficient order placement<br/>
-    • Implemented production level purchase and sell rest APIs with node.js and express<br/>
-    • Worked on baskets and SIP module<br/>`,
+    name: 'Disorder of Creation',
+    designation: 'Full Stack Dev (Freelance)',
+    detail: `• Developed complete e-commerce website including client side application and admin side application.<br/>
+    • Implemented product page, cart page, order page with order status, Products include variants and authentication including google.<br/>
+    • Category nesting is implemented.<br/>
+    • Complete mobile responsiveness with no external libraries for styling.<br/>
+    • Technologies: React, React-Redux, Express, Node.js, MongoDB<br/>`,
     imageUrl:
-      'https://youthincmag.com/wp-content/uploads/2018/04/apple-office.jpg',
+      'https://cdn.shopify.com/s/files/1/2617/4984/collections/Disorder_of_Creation_1200x1200.jpg?v=1668494770',
       startDate:'Mar 21',
   endDate:'Arp 22'
   },
   {
-    name: 'Amazon',
-    designation: 'Software Engineer 1',
-    detail: `Front-end<br/>
-    • Developed complete gold purchase website from scratch. <a className='membershipLink' href='https://gold.spennyapp.com'>link</a><br/>
-    • Handled authentication, gold purchase APIs, payments and edge cases using Nextjs, React and Redux<br/>
-    • Built completely responsive UI/UX using CSS and theme-ui<br/>
-    Back-end<br/>
-    • Designed micro-service architecture for different modules<br/>
-    • Asset baskets include queue architecture (rabbit-mq) for efficient order placement<br/>
-    • Implemented production level purchase and sell rest APIs with node.js and express<br/>
-    • Worked on baskets and SIP module<br/>`,
+    name: 'Laptop Nerd',
+    designation: 'Founder',
+    detail: `• Laptopnerd is a laptop review and information website.<a className='membershipLink' href='https://laptopnerd.in'>link</a><br/>
+    • 10+ articles are ranked on Google’s front page. 20+ articles are on 2nd page on Google.<br/>
+    • Designed complete UI/UX from scratch.<br/>
+    • Managing on-page and off-page SEO. Leading the team and handling smooth running of the website.<br/>`,
     imageUrl:
-      'https://static.toiimg.com/thumb/msid-98031821,width-400,resizemode-4/98031821.jpg',
+      'https://laptopnerd.in/wp-content/uploads/2022/07/All-reviewsallTopLaptops.png',
       startDate:'Mar 21',
   endDate:'Arp 22'
   },
@@ -116,6 +99,91 @@ const arr = [
   },
 ]
 
+const projects = [
+  {
+    name: 'Netflix Profile',
+    designation: 'THIS',
+    detail: `• Build the "Robot-server" which provides a all-in-one test running platform.<br/>
+    • Created the UI which facilitates to check multiple microservices.<br/>
+    • Worked on the Vendor-side simulation tool which improves delivery times by 1 to 2 months.<br/>
+    • Deployed the entire simulation tool and the robot-server as a microservice using Docker and Kubernetes.<br/>
+    • The languages used in the above projects include Python, Java and robot-framework<br/>`,
+    imageUrl:
+      'https://imageio.forbes.com/specials-images/imageserve/5ff3492e056b3292f9c39749/0x0.jpg?format=jpg&width=1200',
+    startDate:'Jan 23',
+  endDate:'Present'
+  },
+  {
+    name: 'Spenny (YC 2020)',
+    designation: 'Full stack Intern',
+    detail: `Front-end<br/>
+    • Developed complete gold purchase website from scratch. <a className='membershipLink' href='https://gold.spennyapp.com'>link</a><br/>
+    • Handled authentication, gold purchase APIs, payments and edge cases using Nextjs, React and Redux<br/>
+    • Built completely responsive UI/UX using CSS and theme-ui<br/>
+    Back-end<br/>
+    • Designed micro-service architecture for different modules<br/>
+    • Asset baskets include queue architecture (rabbit-mq) for efficient order placement<br/>
+    • Implemented production level purchase and sell rest APIs with node.js and express<br/>
+    • Worked on baskets and SIP module<br/>`,
+    imageUrl:
+      'https://i.ytimg.com/vi/h8d70SFOqZ4/maxresdefault.jpg',
+      startDate:'Mar 21',
+      endDate:'Arp 22'
+    },
+  {
+    name: 'Flodata Analytics',
+    designation: 'Frontend Intern',
+    detail: `• Worked on Delhi LDRA map dashboard.<br/>
+    • Rendered polygons, tool tips and colours according to parameters using leafletjs, React-leaflet, React and css<br/>
+    • Developed US ancestry map dashboard using AnyCharts and vanilla JavaSfcript<br/>`,
+    imageUrl:
+      'https://media.licdn.com/dms/image/C4D16AQH3A6VmJZoutQ/profile-displaybackgroundimage-shrink_200_800/0/1654535134753?e=2147483647&v=beta&t=oFfzbVd319269JS3IDNbstPjMpuwST0zlgieIIabnzY',
+      startDate:'Mar 21',
+  endDate:'Arp 22'
+  },
+  {
+    name: 'Disorder of Creation',
+    designation: 'Full Stack Dev (Freelance)',
+    detail: `• Developed complete e-commerce website including client side application and admin side application.<br/>
+    • Implemented product page, cart page, order page with order status, Products include variants and authentication including google.<br/>
+    • Category nesting is implemented.<br/>
+    • Complete mobile responsiveness with no external libraries for styling.<br/>
+    • Technologies: React, React-Redux, Express, Node.js, MongoDB<br/>`,
+    imageUrl:
+      'https://cdn.shopify.com/s/files/1/2617/4984/collections/Disorder_of_Creation_1200x1200.jpg?v=1668494770',
+      startDate:'Mar 21',
+  endDate:'Arp 22'
+  },
+  {
+    name: 'Laptop Nerd',
+    designation: 'Founder',
+    detail: `• Laptopnerd is a laptop review and information website.<a className='membershipLink' href='https://laptopnerd.in'>link</a><br/>
+    • 10+ articles are ranked on Google’s front page. 20+ articles are on 2nd page on Google.<br/>
+    • Designed complete UI/UX from scratch.<br/>
+    • Managing on-page and off-page SEO. Leading the team and handling smooth running of the website.<br/>`,
+    imageUrl:
+      'https://laptopnerd.in/wp-content/uploads/2022/07/All-reviewsallTopLaptops.png',
+      startDate:'Mar 21',
+  endDate:'Arp 22'
+  },
+  {
+    name: 'Uber',
+    designation: 'Software Engineer 1',
+    detail: `Front-end<br/>
+    • Developed complete gold purchase website from scratch. <a className='membershipLink' href='https://gold.spennyapp.com'>link</a><br/>
+    • Handled authentication, gold purchase APIs, payments and edge cases using Nextjs, React and Redux<br/>
+    • Built completely responsive UI/UX using CSS and theme-ui<br/>
+    Back-end<br/>
+    • Designed micro-service architecture for different modules<br/>
+    • Asset baskets include queue architecture (rabbit-mq) for efficient order placement<br/>
+    • Implemented production level purchase and sell rest APIs with node.js and express<br/>
+    • Worked on baskets and SIP module<br/>`,
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMLdEPeumjNOhTKtNYjviEqb10yjnj8P39_Q&usqp=CAU',
+      startDate:'Mar 21',
+  endDate:'Arp 22'
+  },
+]
 
 const skills = [
   {
@@ -201,7 +269,7 @@ const Home: NextPage = () => {
             <section className="md:space-y-24">
               <Row title={'Experience'} movies={arr} type='square'/>
               <Row title={'Skills'} movies={skills} type='circle'/>
-              <Row title={'Projects'} movies={arr} type='square'/>
+              <Row title={'Projects'} movies={projects} type='square'/>
               <Row title={'Achievements'} movies={arr} type='square'/>
 
             </section>
