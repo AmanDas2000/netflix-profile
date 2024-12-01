ada34b9a19f4593d14f0e3f5d1bf64fa2427b4b3
Aman Das <69381563+AmanDas2000@users.noreply.github.com>
2024-12-01 16:16:08 +0000

    updated photo link

--- personal-info/info.js
+++ personal-info/info.js
@@ -266,7 +266,7 @@ export const personalDetail={
   name:"Aman Das",
   designation:"Software Developer 1 @ Oracle",
   detail:"I am a full stack developer currently working with great peers at Oracle, constantly learning and evolving at software development and I ❤️ JavaScript.",
-  imageUrl:"https://instagram.fhyd2-2.fna.fbcdn.net/v/t51.2885-19/433991940_7209001492530819_7588030055608565208_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_ht=instagram.fhyd2-2.fna.fbcdn.net&_nc_cat=103&_nc_ohc=nPznYh1nNPgQ7kNvgHEqFWK&_nc_gid=6c95300b75484ec9b5b0285927bd3bb9&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AYA-fFL43nja6bZiH5iVdiAamIlnB1axRfVfeeHZ21nA9Q&oe=67525CDD&_nc_sid=7a9f4b"
+  imageUrl:"https://photos.google.com/share/AF1QipMbITIPLOqbEt-R2thkoSX7g-DXy9ZfUrzh7Qx7zB-1fxxSqYn6FaR81pwyAOycqQ/photo/AF1QipOsiFdqnolwQl8rdYCVp6UUayCZvogQ5fhtZVkS?key=d002YjRVOFc0UENlUXZGNURsdTQwWWswRUg3d3Zn"
 }
 
 export const personalLinks={
